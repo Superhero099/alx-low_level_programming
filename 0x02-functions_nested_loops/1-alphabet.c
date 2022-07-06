850ff78ba700f1fe077b11beb9889c675844386b
Iroayo 'Juwon <juwonloiroayo@gmail.com>
2022-07-05 18:24:42 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while(i <= 122)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
