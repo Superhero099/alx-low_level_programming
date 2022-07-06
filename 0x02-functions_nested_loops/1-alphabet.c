205b0f819e851adf688f22c9487516f50c2aaf88
Iroayo 'Juwon <juwonloiroayo@gmail.com>
2022-07-05 18:38:34 -0700

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,15 @@
  * Return: Always 0.
  */
 
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+
 void print_alphabet(void)
 {
 	int i = 97;
 
-	while(i <= 122)
+	while (i <= 122)
 	{
 		_putchar(i);
 		i++;
