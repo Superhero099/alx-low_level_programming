b1ef4f4941b8c2e1ed8c452061a1e020c48830da
Iroayo 'Juwon <juwonloiroayo@gmail.com>
2022-07-18 16:42:35 -0700

    task 0 correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include main.h
 
 /**
- * _memset: fills the first @n byte of the memory
+ * _memset - fills the first @n byte of the memory
  *
  * @n: byte of the memory areapointed to by @s
  *
@@ -16,7 +16,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for(; i < n; i++)
+	for (; i < n; i++)
 		s[i] = b;
 
 	return (s);
